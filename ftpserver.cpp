09e1414b44646697c819620ec13526ee4dba778d
autumngoat <autumngoat@hotmail.com>
2014-02-10 15:12:55 -0500

    GET works, uses ctrl connection
    I'll clean it up later

--- ftpserver.cpp
+++ ftpserver.cpp
@@ -15,12 +15,84 @@
 
 #define BUFFER 1024
 
+const char eof[] = "EOF";
+
 struct thread_data{
 	int sid;
 	//char *string;
 };
 
 char homeDir[BUFFER];
+
+void get_file(char* filename, int sockid){
+	printf("sock ID is %i\n", sockid);
+	printf("filename is %s\n", filename);
+	
+	//open file and send file status to client
+	char *status = "NULL";
+	FILE* doc = fopen(filename, "rb");
+	
+	//file DOES NOT exist, send file status, end function
+	if (doc == NULL) {
+		printf("%s does NOT exist\n", filename);
+		if (send(sockid, status, (int)strlen(status), 0) < 0){
+			perror("error sending file status to client");
+			close(sockid);
+			exit(EXIT_FAILURE);
+		}
+		return;
+	}
+	//file EXISTS, send file status
+	else{
+		printf("%s EXISTS\n", filename);
+		if (send(sockid, filename, (int)strlen(filename), 0) < 0){
+			perror("error sending file status to client");
+			exit(EXIT_FAILURE);
+		}
+		
+		char data[1024];
+		//failure to receive client file opening status
+		if (recv(sockid, data, sizeof(data), 0) < 0){
+			perror("Error receiving file opening status from server");
+			close(sockid);
+			exit(EXIT_FAILURE);
+		}
+		//client sent failure to open file
+		if (strstr(data, "CANT")) {
+			perror("Client was not able to open file to be ready for receiving\n");
+			close(sockid);
+			exit(EXIT_FAILURE);
+		}
+		else{
+			size_t size;
+			memset(data, '\0', sizeof(data));
+			
+			printf("sending file...\n");
+			do {
+				size = fread(data, 1, sizeof(data), doc);
+				printf("The bytes read are %s\n", data);
+				
+				if (size < 0) {
+					perror("cannot fread() file");
+					exit(EXIT_FAILURE);
+				}
+				
+				//send
+				if (send(sockid, data, size, 0) < 0) {
+					perror("cannot send file");
+					exit(EXIT_FAILURE);
+				}
+			} while (size > 0);
+			printf("DONE sending\n");
+			
+			//send end of file
+			send(sockid, eof, sizeof(eof), 0);
+			
+			//close file
+			fclose(doc);
+		}
+	}
+}
 
 //Prints the message from the clients and writes the same message back to the client
 void *Echo (void *threadargs){
@@ -40,7 +112,6 @@ void *Echo (void *threadargs){
 		}
 		
 		//Tokenize the client's request
-		
 		char *command=(char *) malloc(BUFFER);
 		char *cargs=(char *) malloc(BUFFER);
 		strcpy(command, str);
@@ -52,84 +123,87 @@ void *Echo (void *threadargs){
 		//Switch for the 7 main commands (not including exit). Else is echo
 		if(strcmp(command, "get")==0){
 			printf("Please implement 'get'\n");
+			get_file(cargs, sid);
 		}
-		else if(strcmp(command, "put")==0){
-			printf("Please implement 'put'\n");
-		}
-		else if(strcmp(command, "delete")==0){
-			printf("Please implement 'delete'\n");
-		}
-		else if( (strcmp(command, "ls")==0) && cargs==NULL ){
-			DIR *dir;
-			struct dirent *entry;
-			char lsContents[BUFFER]="";
-			if (getcwd(cwd, sizeof(cwd)) == NULL){
-				perror("Couldn't get current working directory");
-				strcpy(str, "Couldn't get current working directory\n");
+		else {
+			if(strcmp(command, "put")==0){
+				printf("Please implement 'put'\n");
 			}
-			else if ((dir = opendir(cwd)) == NULL){
-				perror("Opening the directory");
-				strcpy(str, "Failed to open directory object\n");
+			else if(strcmp(command, "delete")==0){
+				printf("Please implement 'delete'\n");
 			}
-			else {
-				printf("contents of root:\n");
-				while ((entry = readdir(dir)) != NULL){
-					printf("  %s\n", entry->d_name);
-					strcat(lsContents, entry->d_name);
-					strcat(lsContents, "\n");
+			else if( (strcmp(command, "ls")==0) && cargs==NULL ){
+				DIR *dir;
+				struct dirent *entry;
+				char lsContents[BUFFER]="";
+				if (getcwd(cwd, sizeof(cwd)) == NULL){
+					perror("Couldn't get current working directory");
+					strcpy(str, "Couldn't get current working directory\n");
 				}
-				closedir(dir);
-				strcpy(str, lsContents);
+				else if ((dir = opendir(cwd)) == NULL){
+					perror("Opening the directory");
+					strcpy(str, "Failed to open directory object\n");
+				}
+				else {
+					printf("contents of root:\n");
+					while ((entry = readdir(dir)) != NULL){
+						printf("  %s\n", entry->d_name);
+						strcat(lsContents, entry->d_name);
+						strcat(lsContents, "\n");
+					}
+					closedir(dir);
+					strcpy(str, lsContents);
+				}
 			}
-		}
-		else if(strcmp(command, "cd")==0){
-			if (cargs==NULL){
-				printf("cd missing arguments\n");
-				strcpy(str, "cd error: must have 1 argument\n");
-			}
-			else if (chdir(cargs)!=0){
-				perror("chdir() error");
-				strcpy(str, "Failed to change directory\n");
-			}
-			else{
-				strcpy(str, "Successfully changed the working directory!\n");
-			}
-		}
-		else if(strcmp(command, "mkdir")==0 ){
-			if (cargs==NULL){
-				strcpy(str, "mkdir error: must have 1 argument\n");
-			}
-			else{
-				if (mkdir(cargs, S_IRWXU|S_IRGRP|S_IXGRP) != 0){
-					perror("mkdir() error");
-					strcpy(str, "mkdir failed to execute\n");
+			else if(strcmp(command, "cd")==0){
+				if (cargs==NULL){
+					printf("cd missing arguments\n");
+					strcpy(str, "cd error: must have 1 argument\n");
+				}
+				else if (chdir(cargs)!=0){
+					perror("chdir() error");
+					strcpy(str, "Failed to change directory\n");
 				}
 				else{
-					strcpy(str, cargs);
-					strcat(str, " directory successfully created\n");
+					strcpy(str, "Successfully changed the working directory!\n");
+				}
+			}
+			else if(strcmp(command, "mkdir")==0 ){
+				if (cargs==NULL){
+					strcpy(str, "mkdir error: must have 1 argument\n");
+				}
+				else{
+					if (mkdir(cargs, S_IRWXU|S_IRGRP|S_IXGRP) != 0){
+						perror("mkdir() error");
+						strcpy(str, "mkdir failed to execute\n");
+					}
+					else{
+						strcpy(str, cargs);
+						strcat(str, " directory successfully created\n");
+					}
+				}
+				
+			}
+			else if( (strcmp(str, "pwd")==0) && cargs==NULL ){
+				if (getcwd(str, sizeof(str)) == NULL){
+					perror("pwd error");
+					strcpy(str, "pwd failed\n");
+				}
+				else{
+					printf("CWD is: %s\n", str);
 				}
 			}
 			
-		}
-		else if( (strcmp(str, "pwd")==0) && cargs==NULL ){
-			if (getcwd(str, sizeof(str)) == NULL){
-				perror("pwd error");
-				strcpy(str, "pwd failed\n");
-			}
 			else{
-				printf("CWD is: %s\n", str);
+				printf("Unrecognised command: %s\n", str);
 			}
-		}
-		
-		else{
-			printf("Unrecognised command: %s\n", str);
-		}
-		
-		//write back to the client
-		wCheck=write(sid, str, BUFFER);
-		if (wCheck<0){
-			perror("write\n");
-			exit(-7);
+			
+			//write back to the client
+			wCheck=write(sid, str, BUFFER);
+			if (wCheck<0){
+				perror("write\n");
+				exit(-7);
+			}
 		}
 	}
 	return NULL;
