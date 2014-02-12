d9d60e19ff3840b708a725c7a7509fab614fa670
sachiksy <sachiksy@uga.edu>
2014-02-12 18:40:53 -0500

    Client and server both updated with Hostname and GET. Minor commenting tweaks. README updated

--- ftpclient.cpp
+++ ftpclient.cpp
@@ -8,20 +8,28 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/uio.h>
+#include <netdb.h>
+#include <fcntl.h>
+
 using namespace std;
 
+#define BUFFER 1024
+
+const char eof[] = "EOF";
+
 int main(int argc, char *argv[]){
-
 	int sock, i;
-	struct sockaddr_in saddr;
-	char buf[1024];
-	struct in_addr inp;
+	//struct sockaddr_in saddr;
+	struct addrinfo hints, *results, *j;
+	char buf[BUFFER];
+	//struct in_addr inp;
 
+	//check for proper command args
 	if (argc!=3){
-		printf("Usage: need a server IP address and server port number\n");
-		exit(1);
+		printf("Usage: %s <server hostname> <port number>\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
-	
+	/*
 	inet_aton(argv[1], &inp);
 	saddr.sin_family = AF_INET;
 	saddr.sin_addr.s_addr = inp.s_addr;
@@ -36,25 +44,136 @@ int main(int argc, char *argv[]){
 	if (connect(sock, (const struct sockaddr *)&saddr, sizeof(saddr))<0){
 		perror("connection");
 		exit(3);
+	} */
+	
+	//obtain address(es) matching hostname and port number
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;			//IPv4
+	hints.ai_socktype = SOCK_STREAM;	//TCP socket
+	
+	//getaddrinfo(hostname, port number, address specifications, start of linked list of address structs)
+	if (getaddrinfo(argv[1], argv[2], &hints, &results) < 0) {
+		perror("Cannot resolve the address");
+		exit(EXIT_FAILURE);
 	}
 	
-	while(strcmp(buf, "exit")!=0){
-		printf("myftp>");
-		cin.getline(buf, 1024);
-		i=write(sock, buf, strlen(buf));
-		if (i<0){
-			perror("write");
-			exit(4);
+	//try each address until successful connection
+	for (j = results; j != NULL; j = j->ai_next) {
+		//if socket creation fail, continue
+		if ((sock = socket(j->ai_family, j->ai_socktype, j->ai_protocol)) < 0) {
+			continue;
 		}
-	
-		i=read(sock, buf, 1024);
-		if (i<0){
-			perror("read");
-			exit(5);
+		//connection success, break
+		if (connect(sock, j->ai_addr, j->ai_addrlen) != -1) {
+			break;
 		}
+		
+		close(sock);
+	}
+	//no successful address
+	if (j == NULL) {
+		perror("Cannot connect\n");
+		exit(EXIT_FAILURE);
+	}
 	
+	freeaddrinfo(results);
+	
+	//user input handling
+	while(strcmp(buf, "exit") != 0){
+		printf("myftp> ");
+		cin.getline(buf, BUFFER);
+		
+		// RECV/SEND for command: GET
+		if (strstr(buf, "get")) {
+			//send get <filename> to server
+			i=write(sock, buf, strlen(buf));
+			if (i<0){
+				perror("write");
+				exit(4);
+			} //if (i<0)
+
+			//recv File status
+			memset(buf, '\0', BUFFER);
+			if (recv(sock, buf, BUFFER, 0) < 0){
+				perror("error receiving file status from server");
+				exit(EXIT_FAILURE);
+			} //if (recv(sock, buf, BUFFER, 0) < 0)
+			printf("client received %s\n", buf);
+			
+			//file does not exist, print to user, loop back to prompt
+			if (strstr(buf, "NULL")) {
+				printf("Sorry, the file you wanted does not exist.\n");
+			} //if (strstr(buf, "NULL"))
+			
+			//file does exist, create data socket/bind/listen/accept, send connection status
+			else {
+				printf("client knows %s EXISTS\n", buf);
+				
+				size_t size;
+				char data[BUFFER];
+				memset(data, 0, sizeof(data));
+				//overwrite existing file or create new file
+				FILE* doc = fopen(buf, "wb");
+				
+				char *openfile;
+				//cannot open file to write
+				if (doc == NULL) {
+					openfile = "CANT";
+					//send file status, end
+					if (send(sock, buf, sizeof(buf), 0) < 0) {
+						perror("Cannot send file opening status to server\n");
+						close(sock);
+						exit(EXIT_FAILURE);
+					} //if (send(sock, buf, sizeof(buf), 0) < 0)
+					perror("Cannot open file to be written\n");
+					close(sock);
+					exit(EXIT_FAILURE);
+				} //if (doc == NULL)
+				//we are ready to receive
+				else {
+					openfile = "GOOD";
+					//send GOOD TO GO, let's start sending that file
+					if (send(sock, buf, sizeof(buf), 0) < 0) {
+						perror("Cannot send ready to receive clearance to server\n");
+						close(sock);
+						exit(EXIT_FAILURE);
+					} //if (send(sock, buf, sizeof(buf), 0) < 0)
+					
+					printf("opened file to write\n");
+					while ((size = recv(sock, data, sizeof(data), 0)) > 0) {
+					printf("receiving...\n");
+						//if EOF, break
+						if ((strcmp(data, eof)) == 0) {
+							printf("end of file matey\n");
+							break;
+						} //if ((strcmp(data, eof)) == 0)
+						fwrite(data, 1, BUFFER, doc);
+					} //while ((size = recv(sock, data, sizeof(data), 0)) > 0)
+					if (size < 0) {
+						perror("problem receiving file from server");
+						exit(EXIT_FAILURE);
+					} //if (size < 0)
+					printf("closing file\n");
+					fclose(doc);
+				} //else
+			} //else
+		} //if (strstr(buf, "get"))
+		// WRITE/READ for commands: LS, PWD
+		else {
+			i=write(sock, buf, strlen(buf));
+			if (i<0){
+				perror("write");
+				exit(4);
+			} //if (i<0)
+
+			i=read(sock, buf, BUFFER);
+			if (i<0){
+				perror("read");
+				exit(5);
+			} //if (i<0)
+		} //else
 		printf("Server reply: %s\n", buf);
-	}
+	} //while(strcmp(buf, "exit")!=0)
 	
 	close(sock);
 
--- ftpserver.cpp
+++ ftpserver.cpp
@@ -10,14 +10,89 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
-using namespace std;
+#include <netdb.h>
+#include <arpa/inet.h>		//needed for creating Internet addresses
+
+#define BUFFER 1024
+
+const char eof[] = "EOF";
 
 struct thread_data{
 	int sid;
 	//char *string;
 };
 
-char homeDir[1024];
+char homeDir[BUFFER];
+
+void get_file(char* filename, int sockid){
+	printf("sock ID is %i\n", sockid);
+	printf("filename is %s\n", filename);
+	
+	//open file and send file status to client
+	char *status = "NULL";
+	FILE* doc = fopen(filename, "rb");
+	
+	//file DOES NOT exist, send file status, end function
+	if (doc == NULL) {
+		printf("%s does NOT exist\n", filename);
+		if (send(sockid, status, (int)strlen(status), 0) < 0){
+			perror("error sending file status to client");
+			close(sockid);
+			exit(EXIT_FAILURE);
+		}
+		return;
+	}
+	//file EXISTS, send file status
+	else{
+		printf("%s EXISTS\n", filename);
+		if (send(sockid, filename, (int)strlen(filename), 0) < 0){
+			perror("error sending file status to client");
+			exit(EXIT_FAILURE);
+		}
+		
+		char data[1024];
+		//failure to receive client file opening status
+		if (recv(sockid, data, sizeof(data), 0) < 0){
+			perror("Error receiving file opening status from server");
+			close(sockid);
+			exit(EXIT_FAILURE);
+		}
+		//client sent failure to open file
+		if (strstr(data, "CANT")) {
+			perror("Client was not able to open file to be ready for receiving\n");
+			close(sockid);
+			exit(EXIT_FAILURE);
+		}
+		else{
+			size_t size;
+			memset(data, '\0', sizeof(data));
+			
+			printf("sending file...\n");
+			do {
+				size = fread(data, 1, sizeof(data), doc);
+				printf("The bytes read are %s\n", data);
+				
+				if (size < 0) {
+					perror("cannot fread() file");
+					exit(EXIT_FAILURE);
+				}
+				
+				//send
+				if (send(sockid, data, size, 0) < 0) {
+					perror("cannot send file");
+					exit(EXIT_FAILURE);
+				}
+			} while (size > 0);
+			printf("DONE sending\n");
+			
+			//send end of file
+			send(sockid, eof, sizeof(eof), 0);
+			
+			//close file
+			fclose(doc);
+		}
+	}
+}
 
 //Prints the message from the clients and writes the same message back to the client
 void *Echo (void *threadargs){
@@ -25,21 +100,20 @@ void *Echo (void *threadargs){
 	struct thread_data *data;
 	data=(struct thread_data *) threadargs;
 	int sid=data->sid;
-	char str[1024];
-	char cwd[1024];
+	char str[BUFFER];
+	char cwd[BUFFER];
 	strcpy(cwd, homeDir);
 	
 	while(strcmp(str, "exit")!=0){
-		memset(str, '\0', 1024);
-		if (read(sid, str, 1024)<0){
+		memset(str, '\0', BUFFER);
+		if (read(sid, str, BUFFER)<0){
 			perror("read");
 			exit(-4);
 		}
 		
 		//Tokenize the client's request
-		
-		char *command=(char *) malloc(1024);
-		char *cargs=(char *) malloc(1024);
+		char *command=(char *) malloc(BUFFER);
+		char *cargs=(char *) malloc(BUFFER);
 		strcpy(command, str);
 		command = strtok (command," ");
 		if (command != NULL){
@@ -49,84 +123,87 @@ void *Echo (void *threadargs){
 		//Switch for the 7 main commands (not including exit). Else is echo
 		if(strcmp(command, "get")==0){
 			printf("Please implement 'get'\n");
+			get_file(cargs, sid);
 		}
-		else if(strcmp(command, "put")==0){
-			printf("Please implement 'put'\n");
-		}
-		else if(strcmp(command, "delete")==0){
-			printf("Please implement 'delete'\n");
-		}
-		else if( (strcmp(command, "ls")==0) && cargs==NULL ){
-			DIR *dir;
-			struct dirent *entry;
-			char lsContents[1024]="";
-			if (getcwd(cwd, sizeof(cwd)) == NULL){
-				perror("Couldn't get current working directory");
-				strcpy(str, "Couldn't get current working directory\n");
+		else {
+			if(strcmp(command, "put")==0){
+				printf("Please implement 'put'\n");
 			}
-			else if ((dir = opendir(cwd)) == NULL){
-				perror("Opening the directory");
-				strcpy(str, "Failed to open directory object\n");
+			else if(strcmp(command, "delete")==0){
+				printf("Please implement 'delete'\n");
 			}
-			else {
-				printf("contents of root:\n");
-				while ((entry = readdir(dir)) != NULL){
-					printf("  %s\n", entry->d_name);
-					strcat(lsContents, entry->d_name);
-					strcat(lsContents, "\n");
+			else if( (strcmp(command, "ls")==0) && cargs==NULL ){
+				DIR *dir;
+				struct dirent *entry;
+				char lsContents[BUFFER]="";
+				if (getcwd(cwd, sizeof(cwd)) == NULL){
+					perror("Couldn't get current working directory");
+					strcpy(str, "Couldn't get current working directory\n");
 				}
-				closedir(dir);
-				strcpy(str, lsContents);
+				else if ((dir = opendir(cwd)) == NULL){
+					perror("Opening the directory");
+					strcpy(str, "Failed to open directory object\n");
+				}
+				else {
+					printf("contents of root:\n");
+					while ((entry = readdir(dir)) != NULL){
+						printf("  %s\n", entry->d_name);
+						strcat(lsContents, entry->d_name);
+						strcat(lsContents, "\n");
+					}
+					closedir(dir);
+					strcpy(str, lsContents);
+				}
 			}
-		}
-		else if(strcmp(command, "cd")==0){
-			if (cargs==NULL){
-				printf("cd missing arguments\n");
-				strcpy(str, "cd error: must have 1 argument\n");
-			}
-			else if (chdir(cargs)!=0){
-				perror("chdir() error");
-				strcpy(str, "Failed to change directory\n");
-			}
-			else{
-				strcpy(str, "Successfully changed the working directory!\n");
-			}
-		}
-		else if(strcmp(command, "mkdir")==0 ){
-			if (cargs==NULL){
-				strcpy(str, "mkdir error: must have 1 argument\n");
-			}
-			else{
-				if (mkdir(cargs, S_IRWXU|S_IRGRP|S_IXGRP) != 0){
-					perror("mkdir() error");
-					strcpy(str, "mkdir failed to execute\n");
+			else if(strcmp(command, "cd")==0){
+				if (cargs==NULL){
+					printf("cd missing arguments\n");
+					strcpy(str, "cd error: must have 1 argument\n");
+				}
+				else if (chdir(cargs)!=0){
+					perror("chdir() error");
+					strcpy(str, "Failed to change directory\n");
 				}
 				else{
-					strcpy(str, cargs);
-					strcat(str, " directory successfully created\n");
+					strcpy(str, "Successfully changed the working directory!\n");
+				}
+			}
+			else if(strcmp(command, "mkdir")==0 ){
+				if (cargs==NULL){
+					strcpy(str, "mkdir error: must have 1 argument\n");
+				}
+				else{
+					if (mkdir(cargs, S_IRWXU|S_IRGRP|S_IXGRP) != 0){
+						perror("mkdir() error");
+						strcpy(str, "mkdir failed to execute\n");
+					}
+					else{
+						strcpy(str, cargs);
+						strcat(str, " directory successfully created\n");
+					}
+				}
+				
+			}
+			else if( (strcmp(str, "pwd")==0) && cargs==NULL ){
+				if (getcwd(str, sizeof(str)) == NULL){
+					perror("pwd error");
+					strcpy(str, "pwd failed\n");
+				}
+				else{
+					printf("CWD is: %s\n", str);
 				}
 			}
 			
-		}
-		else if( (strcmp(str, "pwd")==0) && cargs==NULL ){
-			if (getcwd(str, sizeof(str)) == NULL){
-				perror("pwd error");
-				strcpy(str, "pwd failed\n");
-			}
 			else{
-				printf("CWD is: %s\n", str);
+				printf("Unrecognised command: %s\n", str);
 			}
-		}
-		
-		else{
-			printf("Unrecognised command: %s\n", str);
-		}
-		
-		//write back to the client
-		wCheck=write(sid, str, 1024);
-		if (wCheck<0){
-			perror("write\n");
-			exit(-7);
+			
+			//write back to the client
+			wCheck=write(sid, str, BUFFER);
+			if (wCheck<0){
+				perror("write\n");
+				exit(-7);
+			}
 		}
 	}
 	return NULL;
@@ -137,37 +214,51 @@ int main(int argc, char *argv[]){
 	//kills zombie children
 	signal(SIGCHLD, SIG_IGN);
 
-	int sockid, client;
-	unsigned int addrlen = 0;
-	char buf[1024];
-	struct sockaddr_in saddr;
+	struct addrinfo hints, *results;
+	int sockid, client, reuse;
 	int numThreads=2; //tk
 	pthread_t threads[numThreads];//tk
 	struct thread_data data_arr[numThreads];//tk
 	
 	//checks for command-line params
 	if(argc!=2){
-		printf("Usage: need a port number\n");
-		exit(-1);
+		printf("Usage: %s <port number>\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
 	
-	//opens socket
-	sockid=socket(PF_INET, SOCK_STREAM, 0);
-	if (sockid<0){
-		perror("socket");
-		exit(-2);
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET;				//IPv4
+	hints.ai_socktype = SOCK_STREAM;		//TCP
+	hints.ai_flags = AI_PASSIVE;			//use current IP
+	
+	if (getaddrinfo(NULL, argv[1], &hints, &results) < 0) {
+		perror("Cannot resolve the address\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	//create socket file descriptor
+	sockid = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+	
+	//check for socket creation failure
+	if (sockid < 0) {
+		perror("Cannot open socket\n");
+		exit(EXIT_FAILURE);
 	}
 	
 	printf("got a socket number: %d\n", sockid);
 	
-	saddr.sin_family=AF_INET;
-	saddr.sin_addr.s_addr=htons(INADDR_ANY);
-	saddr.sin_port=htons(atoi(argv[1]));
+	//port reuse
+	reuse = 1;
+	if (setsockopt(sockid, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(int)) < 0) {
+		perror("Cannot set reuse option for socket\n");
+		exit(EXIT_FAILURE);
+	}
 	
-	//binds socket
-	if(bind(sockid, (struct sockaddr *) &saddr, sizeof(saddr))<0){
-		perror("bind");
-		exit(-3);
+	//bind socket to given port
+	if (bind(sockid, results->ai_addr, results->ai_addrlen) < 0) {
+		perror("Cannot bind socket to given port\n");
+		close(sockid);
+		exit(EXIT_FAILURE);
 	}
 	
 	//initialize home directory
@@ -175,15 +266,27 @@ int main(int argc, char *argv[]){
 		perror("Home Directory init error");
 	}
 	
-	listen(sockid, 0);
+	//listen (queue of 5) for incoming connections
+	if (listen(sockid, 5) < 0) {
+		perror("Cannot listen to socket\n");
+		close(sockid);
+		exit(EXIT_FAILURE);
+	}
 	
 	//Accepts a client and calls the echo function
 	int m; //tk
+	//store details about the client who has connected
+	struct sockaddr_in saddr;
+	unsigned int addrlen = sizeof(saddr);
 	while (1){
-		//check if connection is accepted
-		if((client=accept(sockid, (struct sockaddr *) &saddr, &addrlen))<=0){
-			continue;
+		//accept and create a separate socket for client(s)
+		printf("trying to connect\n");
+		if ((client=accept(sockid, (struct sockaddr *) &saddr, &addrlen)) < 0) {
+			perror("Cannot open client socket\n");
+			exit(EXIT_FAILURE);
 		}
+		printf("connected\n");
+		
 		data_arr[0].sid=client;
 		m = pthread_create(&threads[0], NULL, Echo, (void *) &data_arr[0]);
 		if (m){
@@ -194,5 +297,4 @@ int main(int argc, char *argv[]){
 	}
 	pthread_exit(NULL); //tk
 	exit(0);
-
 }
